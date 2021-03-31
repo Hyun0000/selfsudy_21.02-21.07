b6c79ceee97d0a3e54c157c0245423337eaefd3b
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-01 03:42:17 +0900

    Add files via upload

--- 3강 object.js
+++ 3강 object.js
@@ -1,3 +1,19 @@
-var memberArray = ['apple', 'banana', 'kiwi'];
-console.log(memberArray[1]);
-// 배열
+var memberarray = ['banana', 'kiwi', 'apple'];
+console.log(memberarray[1]);
+console.log(memberarray[2]);
+console.log('memberarray[1]', memberarray[1]);
+
+var memberobject = {
+    manager: 'banana',
+    developer: 'kiwi',
+    designer: 'apple'
+}
+memberobject.designer = 'appple';
+console.log(memberobject.designer);
+console.log('memberobject.designer', memberobject.designer);
+
+console.log(memberobject['designer']);
+console.log("memberobject['designer']", memberobject['designer']);
+
+delete memberobject.manager
+console.log(memberobject.manager);
