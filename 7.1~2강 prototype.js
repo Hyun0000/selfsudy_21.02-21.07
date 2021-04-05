7f2e2df058dfe1ddf793404acb82865fe1686e7d
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-06 02:30:07 +0900

    Add files via upload

--- 7.1~2강 prototype.js
+++ 7.1~2강 prototype.js
@@ -3,19 +3,18 @@ function person(name, first, second, third) {
     this.first = first;
     this.second = second;
     this.third = third;
-    this.sum = function () {
-        return this.first + this.second + this.third;
-    }
 }
 
+person.prototype.sum = function () {
+    return 'prototype : ' + (this.first + this.second + this.third);
+}
+
+
 let kim = new person('kim', 10, 20, 30);
+kim.sum = function () {
+    return 'modified : ' + (this.first + this.second + this.third);
+}
 let lee = new person('lee', 10, 10, 10);
-let park = new person('park', 10, 15, 100);
 
 console.log(kim.sum());
 console.log(lee.sum());
-console.log(park.sum());
-
-
-console.log(person());
-console.log(new person());
