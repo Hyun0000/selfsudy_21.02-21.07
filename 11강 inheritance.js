2f49270c64e7a8087f7e6007546f7f8062d38512
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-10 23:35:46 +0900

    Add files via upload

--- 11강 inheritance.js
+++ 11강 inheritance.js
@@ -10,12 +10,21 @@ class person {
 }
 
 class personplus extends person {
+    constructor(name, first, second, third) {
+        this.name = name;
+        this.first = first;
+        this.second = second;
+        this.third = third;
+    }
+    sum = function () {
+        return 'this : ' + (this.first + this.second + this.third);
+    }
     avr = function () {
-        return 'prototype : ' + (this.first + this.second) / 2;
+        return 'prototype : ' + (this.first + this.second + this.third) / 3;
     }
 }
 
-let kim = new personplus('kim', 10, 20);
+let kim = new personplus('kim', 10, 20, 30);
 console.log(kim.sum());
 console.log(kim.avr());
 
