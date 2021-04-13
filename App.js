abb5c7edb453f63a59b540dac038766ed858db2c
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-13 14:07:51 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,25 +1,37 @@
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let [글제목, 글제목변경] = useState(['남자 팬티 추천', '남자 폴댄스 추천', '남자 핫요가 추천']);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Wordl! Mr. Kim
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="black-nav">
+        <div>개발 blog</div>
+      </div>
+
+      <div className="list">
+        <h3>{글제목[0]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h3>{글제목[1]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+
+      <div className="list">
+        <h3>{글제목[2]}</h3>
+        <p>2월 17일 발행</p>
+        <hr />
+      </div>
+
     </div>
-  );
+  )
 }
 
 export default App;
