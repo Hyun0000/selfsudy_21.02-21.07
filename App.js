f4b656d43b9081220321e70d8f4a66e675a4f108
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-17 03:15:59 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/*eslint-disable*/
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
@@ -5,15 +6,16 @@ import './App.css';
 function App() {
 
   let [글제목, 글제목변경] = useState(['남자 팬티 추천', '남자 폴댄스 추천', '남자 핫요가 추천']);
+  let [따봉, 따봉변경] = useState(0);
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-
+      <button>이름변경</button>
       <div className="list">
-        <h3>{글제목[0]}</h3>
+        <h3>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>😊</span> <span>{따봉}</span> </h3>
         <p>2월 17일 발행</p>
         <hr />
       </div>
