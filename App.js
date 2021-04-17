37e6828d7775529b592967c8f4055661a979e1f2
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-18 03:09:03 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -7,13 +7,24 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 팬티 추천', '남자 폴댄스 추천', '남자 핫요가 추천']);
   let [따봉, 따봉변경] = useState(0);
+  // function 제목바꾸기() {
+  //   let newArray = [...글제목];
+  //   newArray[0] = '여자 코트 추천';
+  //   글제목변경(newArray);
+  // }
+
+  function 순서바꾸기() {
+    let number = [...글제목];
+    number.sort();
+    글제목변경(number);
+  }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button>이름변경</button>
+      <button onClick={순서바꾸기}>이름변경</button>
       <div className="list">
         <h3>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>😊</span> <span>{따봉}</span> </h3>
         <p>2월 17일 발행</p>
