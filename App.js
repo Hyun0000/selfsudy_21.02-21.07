f02a0e2ec19a030a3cc4fa789f30a99861957e2b
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-20 02:00:20 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -7,24 +7,24 @@ function App() {
 
   let [글제목, 글제목변경] = useState(['남자 팬티 추천', '남자 폴댄스 추천', '남자 핫요가 추천']);
   let [따봉, 따봉변경] = useState(0);
-  // function 제목바꾸기() {
-  //   let newArray = [...글제목];
-  //   newArray[0] = '여자 코트 추천';
-  //   글제목변경(newArray);
-  // }
-
-  function 순서바꾸기() {
-    let number = [...글제목];
-    number.sort();
-    글제목변경(number);
+  function 제목바꾸기() {
+    let newArray = [...글제목];
+    newArray[0] = '여자 코트 추천';
+    글제목변경(newArray);
   }
+
+  // function 순서바꾸기() {
+  //   let number = [...글제목];
+  //   number.sort();
+  //   글제목변경(number);
+  // }
 
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button onClick={순서바꾸기}>이름변경</button>
+      <button onClick={제목바꾸기}>이름변경</button>
       <div className="list">
         <h3>{글제목[0]} <span onClick={() => { 따봉변경(따봉 + 1) }}>😊</span> <span>{따봉}</span> </h3>
         <p>2월 17일 발행</p>
@@ -43,8 +43,19 @@ function App() {
         <hr />
       </div>
 
+      <Modal></Modal>
+
     </div>
   )
 }
 
+function Modal() {
+  return (
+    <div className="modal">
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
+}
 export default App;
