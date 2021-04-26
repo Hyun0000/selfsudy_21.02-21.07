5e226b8128aec049e6656ec77c46c03e0622d57b
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-04-27 00:15:43 +0900

    Add files via upload

--- Stringapp.java
+++ Stringapp.java
@@ -1,9 +1,9 @@
 
 public class Stringapp {
 	public static void main(String[] args) {
-//		System.out.println("Hello World"); // String (문자가 모여있는 문자열)
-//		System.out.println('Hello World'); // Character
-//		System.out.println("H"); // String
+		System.out.println("Hello World"); // String (문자가 모여있는 문자열)
+//		System.out.println('Hello World'); // Character (한 글자를 나타낼 때 사용)
+		System.out.println("H"); // String
 		System.out.println("Hello "
 				+ "Wordl");
 		System.out.println("Hello \nWorld"); //  \n = new line
---
+++ letter.java
@@ -0,0 +1,9 @@
+public class letter {
+	public static void main(String[] args) {
+		String name = "nepa";
+		System.out.println("Hello, "+name+"...miss you "+name+"");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+}
