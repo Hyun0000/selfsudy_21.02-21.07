bd7647f8807eaf4decda1d7c9a420e6b3270cc4e
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-05-04 01:15:19 +0900

    Add files via upload

---
+++ classapp.java
@@ -0,0 +1,10 @@
+
+public class classapp {
+
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.7));
+		System.out.println(Math.ceil(1.7));
+	}
+
+}
