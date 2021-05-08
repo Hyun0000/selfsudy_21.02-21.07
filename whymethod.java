dbafa2d6ef2666dc27a439ad08dd4c2d8d6f4a8d
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-05-08 09:34:46 +0900

    Add files via upload

---
+++ whymethod.java
@@ -0,0 +1,12 @@
+public class whymethod {
+	public static void printtwotimes() {
+		System.out.println("_");
+		System.out.println("A");
+		System.out.println("A");
+	}
+	public static void main(String[] args) {
+		printtwotimes();
+		printtwotimes();
+		printtwotimes();
+	}
+}
