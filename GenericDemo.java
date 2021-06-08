3893c5df48d00a78d249b3d8a493d4cc13b63147
Hyun0000 <79898454+Hyun0000@users.noreply.github.com>
2021-06-09 00:49:55 +0900

    Add files via upload

--- GenericDemo.java
+++ GenericDemo.java
@@ -1,23 +1,63 @@
 package Generics;
+//
+//class StudentInfo{
+//    public int grade;
+//    StudentInfo(int grade){ this.grade = grade; }
+//}
+//class EmployeeInfo{
+//    public int rank;
+//    EmployeeInfo(int rank){ this.rank = rank; }
+//}
+//class Person{
+//    public Object info;
+//    Person (Object info){ this.info = info; }
+//}
+//public class GenericDemo {
+//    public static void main(String[] args) {
+//        Person p1 = new Person("부장");
+//        EmployeeInfo ei = (EmployeeInfo)p1.info;
+//        System.out.println(ei.rank);
+//    }
+//}
 
-import org.w3c.dom.ls.LSOutput;
-
-class StudentInfo{
-    public int grade;
-    StudentInfo(int grade){ this.grade = grade; }
-}
+//class EmployeeInfo{
+//    public int rank;
+//    EmployeeInfo(int rank){ this.rank = rank; }
+//}
+//class Person<T, S>{
+//    public T info;
+//    public S id;
+//    Person(T info, S id){
+//        this.info = info;
+//        this.id = id;
+//    }
+//}
+//public class GenericDemo {
+//    public static void main(String[] args) {
+//        Integer id = new Integer(1);
+//        Person<EmployeeInfo, Integer> p1 = new Person<EmployeeInfo, Integer>(new EmployeeInfo(1), id);
+//        System.out.println(p1.id.intValue());
+//    }
+//}
 class EmployeeInfo{
     public int rank;
     EmployeeInfo(int rank){ this.rank = rank; }
 }
-class Person{
-    public Object info;
-    Person (Object info){ this.info = info;}
+class Person<T, S>{
+    public T info;
+    public S id;
+    Person(T info, S id){
+        this.info = info;
+        this.id = id;
+    }
+    public <U> void printInfo(U info){
+        System.out.println(info);
+    }
 }
 public class GenericDemo {
     public static void main(String[] args) {
-        Person p1 = new Person("부장");
-        EmployeeInfo ei = (EmployeeInfo) p1.info;
-        System.out.println(ei.rank);
+        EmployeeInfo e = new EmployeeInfo(1);
+        Integer i = 10;
+        Person p1 = new Person(e, i);
     }
 }
